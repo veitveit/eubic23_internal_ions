fd54f966009ad96e1ff5fb67a7ce1408c7a9fada
Micha Birklbauer <micha.birklbauer@gmail.com>
2023-01-17 15:52:10 +0100

    add example usage

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python3
 
 import json
+from typing import Union
+from typing import TextIO
 
 class FragmentAnalyzer:
 
-    def __init__(self, json_file: str) -> None:
+    def __init__(self, json_file: Union[str, TextIO], file_object: bool = False) -> None:
 
-        with open(json_file, "r", encoding = "utf-8") as f:
-            self.data = json.load(f)
-            f.close()
+            if file_object:
+                self.data = json.load(json_file)
+            else:
+                with open(json_file, "r", encoding = "utf-8") as f:
+                    self.data = json.load(f)
+                    f.close()
 
-    def example_analysis():
+    def example_analysis(self):
         print(len(self.data))
+
+def main():
+
+    fa = FragmentAnalyzer("sample.json")
+    fa.example_analysis()
+
+if __name__ == "__main__":
+    main()
