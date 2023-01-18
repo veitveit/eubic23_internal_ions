5f50b6552bba76b77c642ec71a72f319ebb09103
Caroline Linnea Elin Lennartsson <caroline.lennartsson@cpr.ku.dk>
2023-01-18 11:48:27 +0100

    streamlit structure

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,8 @@
 import numpy as np
 import streamlit as st
 import plotly.figure_factory as ff
+import pandas as pd
+
 
 def example_plot():
     x1 = np.random.randn(200) - 2
@@ -24,7 +26,7 @@ def example_plot():
 # main page content
 def main_page():
 
-    title = st.title("title")
+    title = st.title("Internal ions")
 
     text_1 = st.markdown("desc")
 
@@ -32,14 +34,31 @@ def main_page():
                                      type = ["raw"],
                                      help = "desc"
                                     )
+      
 
     identifications_file = st.file_uploader("Upload identification file:",
                                             type = ["mzId"],
                                             help = "desc"
                                            )
+    
+    # RUN fraggannot?
+    run_button = st.button("Analyze Fragments!", help = "Run fragannot")  
+    
+    if run_button:  
+        json_data = run_fraggannot(spectrum_file, identifications_file)  
+        pandas_df = json_pandas(json_data) 
+        
+        seq_length = st.number_input("Minimum sequence lenght")
+        
+        filt_df = filter_json_file(pandas_df, seq_length)
+        
 
-    if st.button("Analyze Fragments!", help = "desc"):
-        plot = st.plotly_chart(example_plot(), use_container_width = True)
+    if st.button("Make some plots!", help = "desc"):
+        plot = st.plotly_chart(example_plot(), use_container_width = True) 
+        
+    
+    
+        
 
 # side bar and main page loader
 def main():
@@ -62,9 +81,49 @@ def main():
 
     logo = st.sidebar.image("img/logo.png", caption = "text")
 
-    doc = st.sidebar.markdown("desc")
-
+    doc = st.sidebar.markdown("desc") 
+    
     main_page()
+    
+
+        
+    
+    
+    
+def run_fraggannot(spectrum_file, identifications_file): 
+    """
+    Funtion that runs fragannot and outputs a json file.  
+    
+    
+    
+    """
+    
+    json_file = "file"
+
+    return json_file
+
+def json_pandas(json_file): 
+    """ 
+    
+    """
+    pd_df = "file"
+    
+    return pd_df
+
+
+def filter_json_file(pd_df, int_frag_len): 
+    """ 
+    
+    """
+    filter_df = "file"
+    
+    return filter_df 
+
+
+
+    
+    
+    
 
 if __name__ == "__main__":
     main()
