cb5fde56e4f7d461593f02502390f0123ffa3fcf
Micha Birklbauer <micha.birklbauer@gmail.com>
2023-01-19 09:16:52 +0100

    add filter function

--- filtering_files.py
+++ filtering_files.py
@@ -2,16 +2,16 @@ import numpy as np
 import pandas as pd
 from typing import List
 
-def filter_json_file(dataframes: List[pd.DataFrame],
-                     start_seq_length: int,
-                     end_seq_length: int,
-                     start_frag_len: int,
-                     end_frag_len: int,
-                     start_mz: float,
-                     end_mz: float,
-                     start_int: float,
-                     end_int: float,
-                     ion_filter: List[bool]) -> List[pd.DataFrame]:
+def filtering_files(dataframes: List[pd.DataFrame],
+                    start_seq_length: int,
+                    end_seq_length: int,
+                    start_frag_len: int,
+                    end_frag_len: int,
+                    start_mz: float,
+                    end_mz: float,
+                    start_int: float,
+                    end_int: float,
+                    ion_filter: List[bool]) -> List[pd.DataFrame]:
     """
 
     """
@@ -25,9 +25,10 @@ def filter_json_file(dataframes: List[pd.DataFrame],
     fragments_df = dataframes[0]
     spectra_df = dataframes[1]
 
-    fragments_df_filtered = fragments_df.query("frag_length > {start_frag_len} & frag_length < {end_frag_len} & frag_mz > {start_mz} & frag_mz < {end_mz} & frag_intensity > {start_int} & frag_intensity < {end_int}".format(start_frag_len, end_frag_len, start_mz, end_mz, start_int, end_int))
-    spectra_df_filtered = spectra_df.query("peptide_length > {start_seq_length} & peptide_length < {end_seq_length}".format(start_seq_length, end_seq_length))
+    # filter by ion type
+    fragments_df = fragments_df[fragments_df["frag_code"].str.contains("|".join(ions_considered))]
 
-    fragments_ion_types = fragments_df_filtered[fragments_df_filtered["frag_code"].str.contains("|".join(ions_considered))]
+    fragments_df_filtered = fragments_df.query(f"frag_length > {start_frag_len} & frag_length < {end_frag_len} & frag_mz > {start_mz} & frag_mz < {end_mz} & frag_intensity > {start_int} & frag_intensity < {end_int}")
+    spectra_df_filtered = spectra_df.query(f"peptide_length > {start_seq_length} & peptide_length < {end_seq_length}")
 
-    return [fragments_ion_types, spectra_df_filtered]
+    return [fragments_df_filtered, spectra_df_filtered]
